9e2e7e2c9a678e0d3c3c90b9e2e953403534de59
sayari <loli@loli.monster>
2022-06-02 17:03:54 +0800

    add danmu avatar img

--- src/renderer/components/Danmu.tsx
+++ src/renderer/components/Danmu.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { Avatar } from '@chakra-ui/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styles from '../styles/danmuc.module.scss';
 
 const Danmu = (prop: any) => {
@@ -6,22 +8,46 @@ const Danmu = (prop: any) => {
     ...prop,
   };
   const [isDisplayble, setIsDisplayble] = useState('inline');
+  const [avatarFace, setAvatarFace] = useState('');
   const faceImg = '';
   const changeDisplay = () => {
     setIsDisplayble('none');
   };
   if (data.data.type === 1) {
+    const uid = data.data.origin.info[2][0];
+    console.info(uid);
+    // eslint-disable-next-line promise/no-nesting
+    axios({
+      url: `https://api.live.bilibili.com/live_user/v1/Master/info?uid=${uid}`,
+    })
+      // eslint-disable-next-line func-names
+      // eslint-disable-next-line promise/always-return
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      // eslint-disable-next-line func-names
+      // eslint-disable-next-line promise/always-return
+      .then(function (response1) {
+        setAvatarFace(response1.data.data.info.face);
+        setIsDisplayble('inline');
+      })
+      // eslint-disable-next-line func-names
+      .catch(function (error) {
+        console.log(error);
+      });
     // todo get img from web
   }
-
+  if (data.data.type === 2) {
+    setAvatarFace(data.data.origin.data.face);
+    setIsDisplayble('inline');
+  }
   return (
     <div className={styles.danmuContainer}>
       <img
         alt=""
         className={styles.avatar}
         style={{ display: isDisplayble }}
+        key={avatarFace}
         onError={changeDisplay}
-        src={data.data.type === 2 ? data.data.origin.data.face : ''}
+        src={avatarFace}
       />
       <div className={styles.fansAndNickname}>
         <div className={styles.nickname}> {data.nickname}</div>
